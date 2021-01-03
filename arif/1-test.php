8c90af95db5d2fc90e722110d4cf38abeff9587c
Rabbani1999 <mdarifrabbani279349@gmail.com>
2021-01-03 20:28:59 +0600

    php string/ comment

---
+++ arif/1-test.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>my first php page</h1>
+
+    <?php echo 'hello word';
+    
+    // this is the php comment
+    # this is the also php comment
+
+    /*
+     this is the multiline comment in the php 
+    we learn php beasuce laravel is depend on php_check_syntax
+    */
+    
+    $text =" Bangladesh";
+    echo "I love $text";
+
+    $x =5;
+    $y= 6;
+    echo $x + $y;
+    
+  
+    
+    ?>
+
+</body>
+</html>
