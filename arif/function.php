2996420dcb06ceced6d8dc5982d16f1d779c4913
Rabbani1999 <mdarifrabbani279349@gmail.com>
2021-01-04 23:40:54 +0600

    update

--- arif/function.php
+++ arif/function.php
@@ -1,3 +1,4 @@
+<?php declare(strict_types=1);  ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -21,8 +22,34 @@
     function number(int $a, int $b){
         return $a+$b;
     }
-    echo number(5, 20)
+    echo number(5, 20);
 
+
+    function sum(int $a , int $b){
+       $c = $a+$b;
+       return $c; 
+    }
+    echo "<br>";
+    echo "sum : ".sum(5,10);
+
+
+    function add_num(&$value){
+        $value+=6;
+    }
+     $sum =2;
+     add_num($sum);
+     echo "<br>". $sum ."<br>";
+
+
+     $test_array = [10,60,70,20,30,40,50];
+     sort($test_array);
+     $arry_lenght =count($test_array);
+
+     for ($i=0; $i < $arry_lenght; $i++) { 
+         echo $test_array[$i ];
+         echo "<br>";
+     }
+     echo $test_array[2];
 
 ?>
 </body>
