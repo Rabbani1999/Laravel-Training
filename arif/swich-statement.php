38cfceaca0a3287fa2e86d6bfad43b126c4aa224
Rabbani1999 <mdarifrabbani279349@gmail.com>
2021-01-04 13:30:17 +0600

    swich statement

---
+++ arif/swich-statement.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+
+    $color = "yellow";
+
+    switch($color){
+        case 'red':
+            echo "your favourite color is ". $color;
+            break;
+        
+            case 'blue':
+                echo "your favourite color no";
+                break;
+                case 'blacl':
+                    echo "your favourite color no";
+                    break;
+                    default:
+                    echo "there is no any favourite color";
+    }
+
+
+
+?>
+</body>
+</html>
