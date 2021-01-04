6381f68b6f3811a7a47b4d7b8f02a1cb00366b84
Rabbani1999 <mdarifrabbani279349@gmail.com>
2021-01-04 12:53:28 +0600

    if else odd even number

--- arif/if-else-game.php
+++ arif/if-else-game.php
@@ -3,20 +3,22 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Student result</title>
+    <title>game</title>
 </head>
 <body>
-    <?php
+    <?php 
 
-    $x = 20;
+    $a = rand(1,10);
 
-    if($x >30){
-        echo 'this is gaterthan 40';
-    }else{
-        echo "this is lessthan 40";
+    echo $a ."<br>";
+    
+    if($a%2==0){
+        echo "even number";
+    }
+    else{
+        echo "odd number";
     }
 
-
-?>
+    ?>
 </body>
 </html>
