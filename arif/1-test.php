1c2e58e3502d78bbdad9f11152141157694c527d
Rabbani1999 <mdarifrabbani279349@gmail.com>
2021-01-04 12:16:12 +0600

    if else statement

---
+++ arif/1-test.php
@@ -0,0 +1,1 @@
+<? php echo 'this the first test from arf' ?>
---
+++ arif/if-else.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Student result</title>
+</head>
+<body>
+    <?php
+
+    $x = 50;
+
+    if($x >30){
+        echo 'this is gaterthan 40';
+    }elseif{
+        echo "this is lessthan 40";
+    }
+
+
+?>
+</body>
+</html>
